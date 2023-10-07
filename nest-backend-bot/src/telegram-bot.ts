b1bcdbc513bb209adc56261dc21ff16f95cb2167
Sparsh Mishra <sparsh.mishra@igniteconnex.com>
2023-10-08 01:54:44 +0530

    Dynamic Location

--- nest-backend-bot/src/telegram-bot.ts
+++ nest-backend-bot/src/telegram-bot.ts
@@ -28,6 +28,7 @@ interface WeatherResponse {
 export class TelegramBotService {
   private bot: TelegramBot;
   private subscribedUsers: Set<number> = new Set<number>();
+  private pendingSubscriptions: Map<number, { chatId: number; username: string }> = new Map<number, { chatId: number; username: string }>();
 
   constructor(private readonly adminService: AdminService, private readonly userService: UserService,) {
     
@@ -49,7 +50,6 @@ export class TelegramBotService {
     console.log("hello");
 
     this.bot.onText(/\/start/, async(msg)=>{
-      console.log("Hello");
       const chatId = msg.chat.id;
       const first_name = msg.from.first_name
 
@@ -57,30 +57,91 @@ export class TelegramBotService {
       
 
     })
+    // this.bot.onText(/\/subscribe/, async (msg) => {
+      
+    //   const chatId = msg.chat.id;
+    //   const userId = msg.from.id;
+    //   const username = msg.from.first_name;
+  
+    //   const existingUser = await this.userService.getUserByChatId(chatId);
+    //   console.log(existingUser);
+      
+  
+    //   if (existingUser) {
+    //     this.bot.sendMessage(chatId, 'You are already registered.');
+    //   } else {
+    //     const user = await this.userService.createUser(userId, username);
+    //     if (user) {
+    //       this.bot.sendMessage(chatId, 'You have been registered.');
+    //       this.subscribedUsers.add(chatId);
+    //       this.sendWeatherUpdate(chatId);
+    //     } else {
+    //       this.bot.sendMessage(chatId, 'Registration failed. Please try again.');
+    //     }
+    //   }
+    // });
+
     this.bot.onText(/\/subscribe/, async (msg) => {
       console.log(msg);
-      
+
       const chatId = msg.chat.id;
       const userId = msg.from.id;
       const username = msg.from.first_name;
-  
-      const existingUser = await this.userService.getUserByChatId(chatId);
-      console.log(existingUser);
+
+      // Prompt the user to share their location
+      this.bot.sendMessage(chatId, 'To subscribe, please share your location:', {
+        reply_markup: {
+          keyboard: [
+            [
+              {
+                text: 'Share Location',
+                request_location: true, // Request the user's location
+              },
+            ]
+          ],
+          resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      });
+
+
+      this.pendingSubscriptions.set(userId, { chatId, username });
+    });
+
+    this.bot.on('location', async (msg) => {
+
+      console.log(msg);
       
-  
-      if (existingUser) {
-        this.bot.sendMessage(chatId, 'You are already registered.');
-      } else {
-        const user = await this.userService.createUser(userId, username);
-        if (user) {
-          this.bot.sendMessage(chatId, 'You have been registered.');
-          this.subscribedUsers.add(chatId);
-          this.sendWeatherUpdate(chatId);
-        } else {
-          this.bot.sendMessage(chatId, 'Registration failed. Please try again.');
+      const userId = msg.from.id;
+      const location = msg.location;
+      const chatId = msg.chat.id;
+
+      const userData = this.pendingSubscriptions.get(userId);
+
+      if (userData) {
+        const latitude = location.latitude;
+        const longitude = location.longitude;
+
+        const weatherData = await this.fetchWeatherData(latitude, longitude);
+        const { chatId, username } = userData;
+        const existingUser = await this.userService.getUserByChatId(chatId);
+
+        // Send the weather update to the user
+        this.bot.sendMessage(chatId, `Weather in your location:\n${weatherData}`);
+
+        if (!existingUser) {
+          await this.userService.createUser(userId, username);
         }
+
+        // Add the user to the list of subscribed users
+        this.subscribedUsers.add(chatId);
+
+        // Remove the user's information from the pending subscriptions
+        this.pendingSubscriptions.delete(userId);
       }
     });
+  
+    
   
     this.bot.onText(/\/unsubscribe/, async (msg) => {
       const chatId = msg.chat.id;
@@ -99,43 +160,11 @@ export class TelegramBotService {
       }
     });
   
-
-    // this.bot.onText(/\/subscribe/, (msg) => {
-    //   const chatId = msg.chat.id;
-    //   if (this.subscribedUsers.has(chatId)) {
-    //     this.bot.sendMessage(chatId, 'You are already subscribed.');
-    //   } else {
-    //     this.subscribedUsers.add(chatId);
-    //     this.bot.sendMessage(
-    //       chatId,
-    //       'You are now subscribed to weather updates here.',
-    //     );
-    //     console.log("calling function");
-        
-    //     this.sendWeatherUpdate(chatId);
-    //   }
-    // });
-
-    // this.bot.onText(/\/unsubscribe/, (msg) => {
-    //   const chatId = msg.chat.id;
-    //   if (this.subscribedUsers.has(chatId)) {
-    //     this.subscribedUsers.delete(chatId);
-    //     this.bot.sendMessage(
-    //       chatId,
-    //       'You have unsubscribed from weather updates.',
-    //     );
-    //   } else {
-    //     this.bot.sendMessage(chatId, 'You are not subscribed.');
-    //   }
-    // });
-    
   }
 
   private async sendWeatherUpdate(chatId: number) {
 
-    const apiKey = this.adminService.getApiKey();
-    console.log(apiKey);
-    
+    const apiKey = this.adminService.getApiKey();    
 
     try {
       const response = await fetch(
@@ -161,7 +190,7 @@ export class TelegramBotService {
   }
 
   private async sendWeatherUpdatesToAll() {
-    // This function sends weather updates to all subscribed users
+
     for (const chatId of this.subscribedUsers) {
       this.sendWeatherUpdate(chatId);
     }
@@ -174,4 +203,36 @@ export class TelegramBotService {
     });
   }
   
+
+  //changes
+  private async fetchWeatherData(latitude: number, longitude: number): Promise<string> {
+    // Make an API request to fetch weather data based on the provided latitude and longitude
+    // Implement the logic to fetch weather data from a weather service of your choice
+
+    // For example, you can use the OpenWeatherMap API or any other weather data provider
+
+    // Replace the following with actual weather data fetching logic
+    const apiKey = this.adminService.getApiKey();
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`,
+      );
+
+      if (!response.ok) {
+        Logger.error('Failed to fetch weather data');
+        return 'Weather data not available';
+      }
+
+      const data: WeatherResponse = (await response.json()) as WeatherResponse;
+
+      const weatherDescription = data.weather[0]?.description;
+      const temperature = (data.main?.temp - 273.15)?.toFixed(2); // Convert to Celsius
+
+      return `Weather:\n${weatherDescription}\nTemperature: ${temperature}°C`;
+    } catch (error) {
+      Logger.error('Error fetching weather data', error);
+      return 'Error fetching weather data';
+    }
+  }
+
 }
